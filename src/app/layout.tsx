017bbf4e543093c2a5bd1644ced5a82537152ee2
sincere1229 <knakamura@donnoba.com>
2026-06-09 10:38:50 +0900

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-     <body>{children}</body>
+     <body>{children}<Analytics /></body>
     </html>
   )
 }
